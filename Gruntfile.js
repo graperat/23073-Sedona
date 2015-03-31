ff25d77720c1244cd011f96aa9dbade672b12281
graperat <graperat@mail.ru>
2015-03-31 19:08:05 +0300

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,24 @@
 module.exports = function(grunt) {
+	grunt.loadNpmTasks("grunt-contrib-less"); 
+
+ grunt.loadNpmTasks("grunt-autoprefixer"); 
+
+ grunt.loadNpmTasks("grunt-combine-media-queries"); 
+
+ grunt.loadNpmTasks("grunt-contrib-cssmin");
 
   grunt.initConfig({
+	cssmin: { 
+		style: { 
+			options: { 
+				keepSpecialComments: 0, 
+				report: "gzip" 
+			}, 
+			files: { 
+				"css/style.min.css": ["css/style.css"] 
+			} 
+		}
+	},
     pkg: grunt.file.readJSON('package.json'),
 
     // Lint Spaces in code
